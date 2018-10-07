6f3fbfcd573daa98cabdecc20299383c0bfb4433
bhavayAnand9 <bhavayanandcse@gmail.com>
2018-10-07 23:32:05 +0530

    project set up

---
+++ src/logic/network.js
@@ -0,0 +1,78 @@
+import { exchange, blockexplorer, pushtx } from 'blockchain.info';
+import bitcoin from 'bitcoinjs-lib';
+import axios from 'axios';
+import Constants from './constants';
+
+const env = {
+    network : 'testnet'
+};
+
+const c_exchange = exchange;
+
+let c_blockexplorer;
+let c_pushtx;
+let c_network;
+
+
+// IF USING A BITCOIN NETWORK UNCOMMENT THIS CODEBLOCK
+
+// switch (env.network) {
+//     case Constants.Networks.Testnet:
+//         c_blockexplorer = blockexplorer.usingNetwork(3);
+//         c_pushtx = pushtx.usingNetwork(3).pushtx;
+//         c_network = bitcoin.networks.testnet;
+//         break;
+//     case Constants.Networks.Bitcoin:
+//         c_blockexplorer = blockexplorer;
+//         c_pushtx = pushtx.pushtx;
+//         c_network = bitcoin.networks.bitcoin;
+//         break;
+//     default:
+//         throw new Error('Unknown network in env file');
+// }
+
+c_blockexplorer = blockexplorer.usingNetwork(3);
+c_pushtx = pushtx.usingNetwork(3).pushtx;
+c_network = bitcoin.networks.testnet;
+
+const getPrice = currency => c_exchange.getTicker({ currency: 'INR' });
+
+const getFee = () => {
+    return axios.get(Constants.Endpoints.BitcoinFees).then((response) => {
+        return (response.data.fastestFee * Constants.Transactions.AverageBytes) / Constants.Bitcoin.Satoshis;
+    }).catch(() => {
+        return 0;
+    });
+};
+
+const broadcast = tx => c_pushtx(tx).then(result => result === Constants.ReturnValues.TransactionSubmitted);
+
+const getUnspentOutputs = (address) => {
+    return c_blockexplorer.getUnspentOutputs(address).then((result) => {
+        return {
+            utxos: result.unspent_outputs,
+            coins: result.unspent_outputs.reduce((a, c) => a + c.value, 0) / Constants.Bitcoin.Satoshis
+        };
+    });
+};
+
+
+const getTransactions = (addresses) => {
+    return c_blockexplorer.getMultiAddress(addresses, {}).then((result) => {
+        return Array.isArray(result.txs) ? result.txs : [];
+    });
+};
+
+
+
+export default {
+    current: c_network,
+    name: env.network,
+    api: {
+        getPrice: getPrice,
+        getFee: getFee,
+        broadcast: broadcast,
+        getUnspentOutputs: getUnspentOutputs,
+        getTransactions: getTransactions,
+    }
+};
